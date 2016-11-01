d94ed9eb34c3dd74047e63b09a5fd2c1d0f18992
floriankirmaier <florian.kirmaier@gmail.com>
2016-11-01 15:05:11 +0100

    update

--- src/main/scala/com/fk/neuralnet/Graph.scala
+++ src/main/scala/com/fk/neuralnet/Graph.scala
@@ -13,25 +13,26 @@ class Graph extends Pane {
     @Bind var innerText = ""
     @Bind var radius = 20.0
     @Bind var image: Image = null
+
     styleClass ::= "corner"
     onClick --> requestFocus
     layoutXY = (posx,posy)
     Δ(layoutXY) <-- Δ(dragDistance)
 
     this <++ new Circle {
-      styleClass ::= "circle"
+      styleClass  ::= "circle"
       this.radius <-- CORNER.radius
-      fill <-- (if(CORNER.image == null) Color.TRANSPARENT else new ImagePattern(CORNER.image))
+      fill        <-- (if(CORNER.image == null) Color.TRANSPARENT else new ImagePattern(CORNER.image))
     }
     this <++ new Label {
       this.text <-- CORNER.text
-      fontSize = 15
-      layoutXY <-- (0,radius + 5) + labXY - (labW / 2, 0)
+      fontSize   =  15
+      layoutXY  <-- (0,radius + 5) + labXY - (labW / 2, 0)
     }
     this <++ new Label {
       this.text <-- CORNER.innerText
-      fontSize = 15
-      layoutXY <-- labXY - labWH / 2
+      fontSize   =  15
+      layoutXY  <-- labXY - labWH / 2
     }
   }
 
@@ -47,7 +48,7 @@ class Graph extends Pane {
     def startPos = startC.pos + dist.normalize * startC.radius
     def endPos   = endC  .pos - dist.normalize * (endC  .radius + 5)
 
-    @Bind val arrow = new Group {
+    this <++ new Group {
       val arrowLength = 20
       styleClass ::= "arrow"
 
@@ -67,10 +68,7 @@ class Graph extends Pane {
     this <++ new Label {
       this.text <-- EDGE.text
       transform <-- Translate((startC.pos + endC.pos) / 2) * Rotate(radToDegrees(dist.angle)) * Translate(-labW / 2, -5 - labH)
-
-      //layoutXY <-- (0,radius + 5) + labXY - (labW / 2, 0)
     }
-    this <++ arrow
   }
 
 
--- src/main/scala/com/fk/neuralnet/View.scala
+++ src/main/scala/com/fk/neuralnet/View.scala
@@ -8,21 +8,21 @@ import Protocol._
 class View extends StackPane with Logic {
 
   @Bind val header = new Label("Exposed! Neural Network in jpro") {
-    styleClass ::= "header"
+    styleClass   ::= "header"
     prefWidthProp := Int.MaxValue
   }
 
   @Bind val  selection = new VBox {
     styleClass ::= "selection"
-    spacing = 5
-    class Box(x: String) extends Button(x) { prefWidthProp = 200}
+    spacing      = 5
+
+    class Box(x: String) extends Button(x) { prefWidthProp = 200 }
     samplenames.map { case (text,name) =>
       <++(new Box(text){ onAction --> openSample(name)})
     }
   }
 
   @Bind var predictionNames = List[String]()
-  predictionNames --> updatePrediction
   @Bind var predictionFields = List.empty[TextField]
   @Bind val  prediction = new ScrollPane(new VBox {
     styleClass ::= "predict"
@@ -32,22 +32,27 @@ class View extends StackPane with Logic {
       prefWidthProp = 200
       onAction --> predict()
     }
+    predictionNames --> updatePrediction
+
+    def updatePrediction(x: List[String]): Unit = {
+      predictionFields = x map { text =>
+        new TextField {
+          promptText = text
+          javafx.scene.layout.VBox.setMargin(this,Insets(5))
+        }
+      }
+    }
 
     children <-- predictButton :: predictionFields
   }) {
     hbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
   }
 
-  def updatePrediction(x: List[String]): Unit = {
-    predictionFields = x map { text =>
-      new TextField { promptText = text; javafx.scene.layout.VBox.setMargin(this,Insets(5))}
-    }
-  }
 
   val leftside = new VBox(selection,prediction) {
     minWidthProp = 200
-    spacing = 50
-    hgrow = Priority.ALWAYS
+    spacing      = 50
+    hgrow        = Priority.ALWAYS
   }
 
   val main = new HBox(leftside,testgraph) {
@@ -63,7 +68,7 @@ class View extends StackPane with Logic {
 
   <++(new VBox(header,main) {
     styleClass ::= "main"
-    spacing = 10
+    spacing      = 10
   })
 
   def updateContent(x: Result) = {
